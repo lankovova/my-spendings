efe8933fd0103fb50286edcdc1e337c968bcad56
lankovova <lanko.vova@gmail.com>
2019-07-07 20:29:23 +0300

    add eslint plugin:react/recommended

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,7 @@ import React from 'react';
 import axios from 'axios';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      text: '',
-    };
-  }
+  state = { text: '' };
 
   async componentDidMount() {
     const res = await axios.get('/hello');
@@ -18,7 +12,14 @@ class App extends React.Component {
   render() {
     const { text } = this.state;
 
-    return <div className="App">{text}</div>;
+    return (
+      <div className="App">
+        <div>{text}</div>
+        <div>
+          {[{ id: 2, v: 2 }, { id: 1, v: 1 }, { id: 3, v: 4 }].map(a => a)}
+        </div>
+      </div>
+    );
   }
 }
 
