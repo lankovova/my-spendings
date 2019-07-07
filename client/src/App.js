1429e1b895966db7d5ecca060ea6de6defa819f4
lankovova <lanko.vova@gmail.com>
2019-07-07 14:10:26 +0300

    add server proxy to client dev server

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return <div className="App">CRA</div>;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: '',
+    };
+  }
+
+  async componentDidMount() {
+    const res = await axios.get('/hello');
+    this.setState({ text: res.data.text });
+  }
+
+  render() {
+    const { text } = this.state;
+
+    return <div className="App">{text}</div>;
+  }
 }
 
 export default App;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const path = require('path');
 const config = require('./config');
 
 const app = express();
@@ -11,8 +12,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const loggerFormat = '[:date[web]] ":method :url" :status :response-time';
 app.use(morgan(loggerFormat));
 
-app.get('/', (req, res) => {
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/hello', (req, res) => {
   res.json({ text: 'Hello there' });
+});
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/client/build/index.html`));
 });
 
 app.listen(config.port, () => console.info(`Server is running on port: ${config.port}`));
